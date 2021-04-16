890e91ed18b81ccd8b402036689b63bd87d73f4c
pandabrand <pandabrand@gmail.com>
2021-04-15 21:21:08 -0500

    change to imagekit url

--- src/_data/home.js
+++ src/_data/home.js
@@ -14,10 +14,14 @@ module.exports = async function() {
 
   const variables = { slug: 'fullpan' };
   const endpoint = process.env.GRAPHQL_URL;
+  const imageKitEndpoint = 'https://ik.imagekit.io/studiofwww/';
+  const spacesUrl = 'https://cc-backs.nyc3.digitaloceanspaces.com/';
 
   try {
     const data = await request(endpoint, pageQuery, variables);
 
+    const imagePath = data.mediaItemBy.mediaItemUrl.substring(spacesUrl.length);
+    data.mediaItemBy['imageKitUrl'] = imageKitEndpoint + imagePath;
     return data.mediaItemBy;
 
   } catch (error) {
