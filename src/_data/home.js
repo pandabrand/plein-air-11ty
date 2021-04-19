b0e6a2aed05468a976f9331e9577a375d03718cb
pandabrand <pandabrand@gmail.com>
2021-04-19 09:15:31 -0500

    img test

--- src/_data/home.js
+++ src/_data/home.js
@@ -1,4 +1,5 @@
 const { request, gql } = require('graphql-request');
+const ImageKit = require("imagekit");
 
 module.exports = async function() {
   const pageQuery = gql`
@@ -17,11 +18,21 @@ module.exports = async function() {
   const imageKitEndpoint = 'https://ik.imagekit.io/studiofwww/';
   const spacesUrl = 'https://cc-backs.nyc3.digitaloceanspaces.com/';
 
+  var imagekit = new ImageKit({
+      publicKey : "public_PMrFGDYsz71y7jDwgaNYL8fQcNo=",
+      privateKey : "private_Xi9yHcVl8f3CgpvW1zNl7zPtf/0=",
+      urlEndpoint : "https://ik.imagekit.io/studiofwww"
+  });
+
   try {
     const data = await request(endpoint, pageQuery, variables);
 
     const imagePath = data.mediaItemBy.mediaItemUrl.substring(spacesUrl.length);
-    data.mediaItemBy['imageKitUrl'] = imageKitEndpoint + imagePath;
+    data.mediaItemBy['imageKitUrl'] = imagekit.url({
+      path: imagePath,
+      endpoint: imageKitEndpoint,
+    });
+
     return data.mediaItemBy;
 
   } catch (error) {
